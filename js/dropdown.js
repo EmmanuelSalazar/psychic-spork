c26661d0e9318e4b59c425c72f338578d537a8dc
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2024-06-11 11:22:53 -0500

    ANIMATION ADD

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,7 +1,13 @@
 $('#dropdown').click(function(){
     if ($('#dropDownMenu').css('display') != 'none') {
+        $('#dropDownMenu').fadeTo('fast', 1);
+
         $('#dropDownMenu').css('display', 'none');
+       
     } else {
+        $('#dropDownMenu').fadeTo('fast', 1);
         $('#dropDownMenu').css('display', 'grid');
+        
+        
     }     
 })
