4b7fd49899a820988cf516f49686954e0401a4f4
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2024-11-06 19:18:02 -0500

    BETTER PRODUCT API

--- js/productos.js
+++ js/productos.js
@@ -1,10 +1,27 @@
-$.getJSON('json/productos.json', function(data){
-    for(var i = 0; i < data.result.length; i++) {
-          out = '<div class="card">' +
-          data.result[i].Nombre
-          + '</div>';
-      }
-       out += "</ul>";
-      $('#productList').html(out);
+$(document).ready(function () {
+  $.getJSON("../json/productos.json", function (data) {
+    var productosJson = data;
+    function mostrarProductos(productos) {
+      var card = $("#productList");
+      $.each(productos, function (i, producto) {
+        console.log(producto.Imagen);
+        var productoHTML =
+          '<div class="card">' +
+          '<img src="' +
+          producto.Imagen +
+          '" alt="">' +
+          '<div class="infoCard"><h2 class="titleCard">' +
+          producto.Nombre +
+          "</h2>" +
+          "<p>$" +
+          producto.Precio +
+          "</p>" +
+          '<button class="btn" onclick="' +
+          producto.Id +
+          '">Comprar</button></div></div>';
+        card.append(productoHTML);
+      });
+    }
+    mostrarProductos(productosJson);
   });
-  
+});
